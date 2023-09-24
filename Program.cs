fbc155b1401de63a1c9233a2967516d8b0cbf9d0
ester <ester@ZETTERLUND>
2023-09-24 22:16:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ChessBoard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej, skriv in en siffra:");
+            int number = int.Parse(Console.ReadLine());
+
+            int EvenOdd = 1;
+
+            for (int row = 0; row < number; row++) 
+            {
+
+                for (int column = 0; column < number; column++ ) 
+                {
+                    if (EvenOdd == 1)
+                    {
+                        Console.OutputEncoding = System.Text.Encoding.Unicode; 
+                        Console.Write("◼︎"); 
+                    }
+                    else
+                    {
+                        Console.OutputEncoding = System.Text.Encoding.Unicode; 
+                        Console.Write("◻︎"); 
+                    }
+                    EvenOdd *= -1;
+
+                }
+                if (number % 2 == 0)
+                {
+                    EvenOdd *= -1;
+                }
+
+                Console.WriteLine();
+            }
+
+
+
+        }
+    }
+}
